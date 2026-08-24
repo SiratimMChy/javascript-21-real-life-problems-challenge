e303eae3182e98e09d96ce5eca113a6625083e40
Siratim Mustakim Chowdhury <siratimmustakimchowdhury123@gmail.com>
2026-08-25 02:24:35 +0600

    Solve Problem 3 in 01-shopping-and-finance

--- 01-shopping-and-finance/Problem3.js
+++ 01-shopping-and-finance/Problem3.js
@@ -1,2 +1,23 @@
+function calculateSalary(employee) {
+      const { name, basicSalary, bonus, tax } = employee;
+    let Gross_Salary = basicSalary + bonus;
+    let Net_Salary = Gross_Salary - tax;
+    return {
+        GrossSalary: Gross_Salary,
+        Bonus: bonus,
+        Tax: tax,
+        NetSalary: Net_Salary
+    }
+}
+
+const employee = {
+    name: "Rahim",
+    basicSalary: 30000,
+    bonus: 5000,
+    tax: 3000
+};
+
+console.log(calculateSalary(employee));
+
 
 
